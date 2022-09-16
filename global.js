29f62f189d796c2735e2a6585c332b59d3a78af9
Lizette Ayala <ayall291@newschool.edu>
2022-09-15 20:32:48 -0400

    Add files via upload

---
+++ global.js
@@ -0,0 +1,86 @@
+
+
+function visitGoFundMe(){
+        window.location='https://www.gofundme.com/f/to-the-family-of-channara-pheapphilly', '_blank';
+    }
+
+function Resource01(){
+        window.location='https://www.gofundme.com/f/to-the-family-of-channara-pheapphilly', '_blank';
+    }
+
+function Resource02(){
+        window.location='https://www.youtube.com/watch?app=desktop&v=brOgDPecPLE&ab_channel=TimeSaved', '_blank';
+    }
+
+function Resource03(){
+        window.location='https://www.change.org/p/bill-lee-clemency-for-quinton-sanders?utm_source=share_petition&utm_medium=custom_url&recruited_by_id=89923f10-66dd-11ea-9320-d5f6abf76c81', '_blank';
+    }
+
+
+function Resource04(){
+        window.location='https://www.change.org/p/life-at-16', '_blank';
+    }
+
+function Resource05(){
+        window.location='https://www.tnvoteyeson3.org/', '_blank';
+    }
+
+function Resource06(){
+        window.location='https://www.change.org/p/exonerate-maurice-mays?recruiter=1272736513&recruited_by_id=37625f60-11b5-11ed-aee8-5d97f6809ae7&utm_source=share_petition&utm_campaign=share_for_starters_page&utm_medium=facebook&fbclid=IwAR2LD0B5b9rzSbKSXFiFnKtA_23OcwJKmB5pGyqn1V9yfyAThkJ2kIehNKI', '_blank';
+    }
+
+function Resource07(){
+        window.location='https://www.participatorydefense.org/', '_blank';
+    }
+
+function Resource08(){
+        window.location='https://www.justiceashealing.org/participatory-defense-1', '_blank';
+    }
+
+function Resource09(){
+        window.location='https://www.law.berkeley.edu/article/a-new-way-forward-participatory-defense-symposium-illuminates-and-inspires/', '_blank';
+    }
+
+function Resource10(){
+        window.location='https://harvardcrcl.org/participatory-defense-what-it-is-and-why-it-deserves-our-attention/', '_blank';
+    }
+
+function Resource11(){
+        window.location='https://www.cdetpush.org/single-project', '_blank';
+    }
+
+function Resource12(){
+        window.location='https://www.siliconvalleydebug.org/', '_blank';
+    }
+
+function Resource13(){
+        window.location='https://www.instagram.com/knoxvillesblackmamasbailout/?fbclid=IwAR1dFTGkMc_3ap7wFL08XDyRB73EAD-pxQ_HgiH7F9PA47rnMZMry5ude-o', '_blank';
+    }
+
+function Resource14(){
+        window.location='https://account.venmo.com/u/Appalachian-CommunityFund', '_blank';
+    }
+
+function Resource15(){
+        window.location='https://www.instagram.com/poptenn865/', '_blank';
+    }
+
+function Resource16(){
+        window.location='https://www.prysm.us/', '_blank';
+    }
+
+function Resource17(){
+        window.location='https://amorri.org/', '_blank';
+    }
+
+function Resource18(){
+        window.location='https://secure.actblue.com/donate/amor-network', '_blank';
+    }
+
+
+
+
+
+
+
+
